807dad586c829a5d5dcca118215fe9bc29eaf6db
gibonifacio <gibonifacio3@gmail.com>
2023-06-29 16:21:54 -0300

    Initial Commit

---
+++ AulaGitHub/AulaGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGitHubApp.swift
+//  AulaGitHub
+//
+//  Created by Giovanna Bonifacho on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
